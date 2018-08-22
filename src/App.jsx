757a710c86aa13a02af888bc1f73915d4ef27c74
Jared Ross <jared.b.ross@gmail.com>
2018-08-22 23:04:15 +0800

    Implement Countdown

--- src/App.jsx
+++ src/App.jsx
@@ -23,15 +23,39 @@ function DialogCreate(props) {
   );
 }
 
+const INTERVAL_POLLING_TIME = 100;
+class Countdown extends React.Component {
+  componentDidMount() {
+    this.interval = window.setInterval(() => this.forceUpdate(), INTERVAL_POLLING_TIME);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+
+  render() {
+    const remaining = Math.floor((this.props.finish - Date.now()) / 1000);
+
+    return (
+      <React.Fragment>
+        {remaining < 1 ? 'Less than 1 second left' : `${remaining} seconds left`}
+      </React.Fragment>
+    );
+  }
+}
+
 function DialogDisplayInner(props) {
   const dialogs = Object.values(props.dialog.dialogs);
 
   return (
     <div className="dialog-item-list">
-      {dialogs.map(({ id, content, start, timeout }) => (
-        <div className="dialog-item">
+      {dialogs.map(({ id, content, startTime, timeout }) => (
+        <div className="dialog-item" key={id}>
           <div className="dialog-item__content">
             {content}
+          </div>
+          <div className="dialog-item__countdown">
+            <Countdown finish={startTime + timeout} />
           </div>
           <button
             className="dialog-item__close"
@@ -61,8 +85,8 @@ function App() {
         <DialogCreate timeout={2000}><div>Dialog 1</div></DialogCreate>
         <DialogCreate timeout={4000}><div>Dialog 2</div></DialogCreate>
         <DialogCreate timeout={6000}><div>Dialog 3</div></DialogCreate>
-        <DialogCreate timeout={8000}><div>Dialog 3</div></DialogCreate>
-        <DialogCreate timeout={10000}><div>Dialog 3</div></DialogCreate>
+        <DialogCreate timeout={8000}><div>Dialog 4</div></DialogCreate>
+        <DialogCreate timeout={10000}><div>Dialog 5</div></DialogCreate>
       </DialogProvider>
     </div>
   );
